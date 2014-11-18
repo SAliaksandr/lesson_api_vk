3ed97fd0c12f8ef396e42d15825a18dccbad8932
isxam <st.maxik@gmail.com>
2014-11-19 00:18:57 +0300

    Part 3; Get current user info;

--- js/app.js
+++ js/app.js
@@ -14,6 +14,21 @@ var Application = {
     run : function()
     {
         $('#app-status').html('Application active');
+
+        // load current user info
+        VK.api('users.get', { fields : 'first_name, last_name, photo_50'  }, this.onUserInfoResponse.bind(this));
+
+
+    },
+
+    onUserInfoResponse  : function(data)
+    {
+        var users = data.response;
+        var currentUser = users[0];
+
+        $('#firstname').text(currentUser.first_name);
+        $('#lastname').text(currentUser.last_name);
+        $('#user-photo').attr('src', currentUser.photo_50);
     }
 
 };
